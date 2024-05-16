e757790f82a0b6bb814728f0b9ce90b71ec56f5e
Leon <leonmorin410@gmail.com>
2024-05-16 23:56:19 +0100

    Check and save date of last update.
    Compare current date with the date of the last update, if different then call the update wallpaper function and save newest update date.

--- venv/dailyWallpaper.py
+++ venv/dailyWallpaper.py
@@ -1,5 +1,6 @@
 import os
 import json
+from datetime import date
 
 wallpapers = {}
 filename = "wallpaper.json"
@@ -7,7 +8,8 @@ filename = "wallpaper.json"
 
 def save_wallpapers():
     with open(filename, "w") as f:
-        json.dump(wallpapers, f)
+        data = {"wallpapers": wallpapers}
+        json.dump(data, f)
 
 
 def load_dir():
@@ -20,10 +22,34 @@ def load_dir():
     else:
         with open(filename, "r") as f:
             saved_walls = json.load(f)
-            if saved_walls != wallpapers:
+            if saved_walls["wallpapers"] != wallpapers:
                 print("Files in directory have changed.")
                 save_wallpapers()
 
 
+def update_wallpaper():
+    print("Updating wallpaper")
+    date_dict = {"date" : str(date.today())}
+    with open(filename) as f:
+        data = json.load(f)
+        data.update(date_dict)
+    json.dump(data, open(filename, "w"))
+
+
+def load_last_update():
+    print("Loading last update")
+    data = json.load(open(filename, "r"))
+    return data.get("date", None)
+
+
+def date_check():
+    print("checking date.")
+    last_update = load_last_update()
+    today = str(date.today())
+    if last_update != today:
+        update_wallpaper()
+
+
 if __name__ == "__main__":
     load_dir()
+    date_check()
