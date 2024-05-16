6684183664dab2705674ca0e44ca9ea51eaa1971
Leon <leonmorin410@gmail.com>
2024-05-16 23:18:28 +0100

    Refactored loading and saving of wallpapers.

--- venv/dailyWallpaper.py
+++ venv/dailyWallpaper.py
@@ -4,23 +4,26 @@ import json
 wallpapers = {}
 filename = "wallpaper.json"
 
+
+def save_wallpapers():
+    with open(filename, "w") as f:
+        json.dump(wallpapers, f)
+
+
 def load_dir():
     files_in_dir = os.listdir("/home/leon/Pictures/wallpapers")
     for i in range(0, len(files_in_dir)):
         wallpapers[str(i)] = files_in_dir[i] #typecast key to string to match json format
-    try:
-        if os.path.exists(filename):
-            with open(filename, "r") as f:
-                saved_walls = json.load(f)
-                if saved_walls != wallpapers:
-                    print("dir has changed")
-                    with open(filename, "w") as f:
-                        json.dump(wallpapers, f)
-        else:
-            with open(filename, "w") as f:
-                json.dump(wallpapers, f)
-    except:
-        print("Failed to load or write data")
+
+    if not os.path.exists(filename):
+        save_wallpapers()
+    else:
+        with open(filename, "r") as f:
+            saved_walls = json.load(f)
+            if saved_walls != wallpapers:
+                print("Files in directory have changed.")
+                save_wallpapers()
 
 
-load_dir()
+if __name__ == "__main__":
+    load_dir()
